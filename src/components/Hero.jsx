b5eb6f9aac7b3fd9f73d987cdd10f016ea446387
esther-kpet <dev@estherkpetemey.com>
2026-01-16 10:08:09 +0000

    Start working on the video animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { SplitText } from 'gsap/all';
+import { useMediaQuery } from 'react-responsive';
 
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     useGSAP(() => {
     const heroSplit = new SplitText('.title', { type: 'chars, words' });
     const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
@@ -36,7 +41,10 @@ const Hero = () => {
         },
     })
     .to('.right-leaf', { y: 200 }, 0)
-    .to('.left-leaf', { y: -200 }, 0);
+    .to('.left-leaf', { y: -200 }, 0)
+
+    const startValue = isMobile ? 'top 50%' :'center 60%';
+    const endValue = isMobile ? '120% top%': 'botton top';
   }, []
   );
   return (
@@ -70,6 +78,17 @@ const Hero = () => {
         </div>
 
     </section>
+
+    <div className='video absolute inset-0'>
+        <video
+            ref={videoRef}
+            src="/videos/input.mp4" 
+            muted
+            playsInline
+            preload='auto'
+            />
+
+    </div>
     </>
   )
 }
