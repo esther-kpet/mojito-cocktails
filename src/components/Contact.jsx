eb7c60ec86924176406001755bc4eb59c833c467
Esther <dev@estherkpetemey.com>
2026-04-17 17:27:35 +0000

    Started the Contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <footer id="contact">
+        <img src="/images/footer-right-leaf.png" alt="leaf-right" id="f-right-leaf" />
+        <img src="/images/footer-left-leaf.png" alt="leaf-left" id="f-left-leaf" />
+
+        <div className='content'>
+            <h2>Where to find us?</h2>
+
+            <div>
+                <h3>Visit our store</h3>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Contact
